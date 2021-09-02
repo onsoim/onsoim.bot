1c3fd2d4e842965bc3a7600b24265efb588b5073
onsoim <onsoim@gmail.com>
2021-09-03 01:03:22 +0900

    [ADD] auto swap platform
    I've added some swapper code depend on its running platform to identify its mode.

--- src/main.py
+++ src/main.py
@@ -2,13 +2,29 @@
 from discord.ext    import commands
 
 
+class Darwin:
+    def __init__(self):
+        self.mode   = 'debug'
+
+class Linux:
+    def __init__(self):
+        self.mode   = 'release'
+
+class Windows:
+    def __init__(self):
+        self.mode   = 'debug'
+
+
 def main():
 
     print("Starting @onsoim.bot")
     bot = commands.Bot(command_prefix='!')
 
+    import platform
+    os = eval(platform.system())()
+
     import json
-    with open('res/channel.json') as f: channel_id = json.load(f)['debug']
+    with open('res/channel.json') as f: channel_id = json.load(f)[os.mode]
 
     @bot.command()
     async def ping(ctx):
