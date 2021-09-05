feda2707f95fbd04112f0056672e08d1b2909931
onsoim <onsoim@gmail.com>
2021-09-06 01:37:50 +0900

    [ADD] init process for gss watchdog
    I've added init process to initialize data file for gss watchdog.

--- src/scraper/GSSWatchdog.py
+++ src/scraper/GSSWatchdog.py
@@ -1,12 +1,20 @@
 
+import json
+
 class GSS:
     def __init__(self):
-        import json
-        with open('data/notice.json') as f: Jnotice = json.load(f)
+        self.fName      = 'data/notice.json'
+        self.init       = {
+            "url": "https://scs.korea.ac.kr/ime/commu/notice.do",
+            "top-notices": [],
+            "notices": {}
+        }
+
+        Jnotice         = self.get_json()
         url             = Jnotice['url']
         prev_notices    = Jnotice['notices']
         prev_top_notices= Jnotice['top-notices']
-        last_notice_num = int(next(iter(prev_notices)))
+        last_notice_num = int(next(iter(prev_notices))) if prev_notices else 0
 
         import requests
         from bs4 import BeautifulSoup
@@ -38,14 +46,7 @@ class GSS:
 
         Jnotice['top-notices']  = top_notices
         Jnotice['notices']      = notices
-
-        with open("data/notice.json", "w") as f:
-            json.dump(
-                Jnotice,
-                f,
-                indent=4,
-                ensure_ascii=False
-            )
+        self.set_json(Jnotice)
 
         self.new    = {
             'top_notices': [ x for x in top_notices if x not in prev_top_notices ],
@@ -54,6 +55,25 @@ class GSS:
 
     def get_new(self):
         return self.new
+
+    def get_json(self):
+        import os
+
+        if not os.path.isfile(self.fName):
+            self.set_json(self.init)
+            return self.init
+
+        with open(self.fName) as f:
+            return json.load(f)
+ 
+    def set_json(self, data):
+        with open(self.fName, "w") as f:
+            json.dump(
+                data,
+                f,
+                indent=4,
+                ensure_ascii=False
+            )
 
     def test(self):
         msg = []
