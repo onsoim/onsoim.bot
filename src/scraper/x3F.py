b141e5ec2e1768118e8fd4eb0e87c8c53b758ba3
onsoim <onsoim@gmail.com>
2021-09-19 21:19:00 +0900

    [ADD] beautifier of 0x3F
    I've added a beautifier of 0x3F, and dumped it.

--- src/scraper/x3F.py
+++ src/scraper/x3F.py
@@ -8,24 +8,53 @@ class x3F:
 
         Jx3F            = self.get_json()
         url             = Jx3F['url']
+        lists           = Jx3F['lists']
 
         from REQUESTS import REQUESTS
         requests = REQUESTS()
 
         res = requests.get(f'{url}/airing')
-        if res: 
+        if res:
             from bs4 import BeautifulSoup
             a = BeautifulSoup(
                 res.text,
                 'html.parser'
             ).select('a')
-            lists = [[ s.text.strip() for s in _.select('strong') ] for _ in a if len(_.select('strong')) > 1 ]
 
-            for list in lists: print(list)
+            for p in [
+                [ s.text.strip() for s in _.select('strong') ]
+                    for _ in a if len(_.select('strong')) > 1 ]:
+                index = p[-1][:-1]
+                if index != "PV":
+                    l = {
+                        'S': 'On going',
+                        'L': 0,
+                        'U': []
+                    } if not p[0] in lists else lists[p[0]]
+
+                    index = int(index)
+                    if l['L'] != index:
+                        l['U'] = list(set(
+                            l['U'] + [ i + 1 for i in range(l['L'], index) ]
+                        ))
+                        l['L'] = index
+                        if len(p) == 3: l['S'] = p[1]
+
+                        Jx3F['lists'][p[0]] = l
+            self.set_json(Jx3F)
 
     def get_json(self):
         with open(self.dName) as f:
             return json.load(f)
+
+    def set_json(self, data):
+        with open(self.dName, "w") as f:
+            json.dump(
+                data,
+                f,
+                indent=4,
+                ensure_ascii=False
+            )
 
 
 if __name__ == "__main__":
