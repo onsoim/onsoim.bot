cd38a48905ab5cc68b32124ab447a215c7fd18c9
onsoim <onsoim@gmail.com>
2021-09-22 23:23:06 +0900

    [ADD] "listup" command of x3F

--- src/command/x3F.py
+++ src/command/x3F.py
@@ -1,9 +1,25 @@
 
 class x3F:
+    def __init__(self):
+        self.dName  = 'data/0x3F.json'
+
+        import json
+        with open(self.dName) as f:
+            self.Jx3F = json.load(f)
+
     def commands(self, args):
         if len(args) ==  0 : return "No arguments"
+        elif args[0] == 'l': return self.get_list()
         else: return args
 
+    def get_list(self):
+        msg, index = "", 0
+        for l in self.Jx3F['lists']:
+            index += 1
+            msg += f'{index}. {l}\n'
+        return msg[:-1]
+
+
 if __name__ == "__main__":
-    print(x3F().commands('a', 'b', 'c', 'd', 'e'))
-    print(x3F().commands('1', '2', '3', '4', '5'))
+    print("('l')")
+    print(x3F().commands('l'))
